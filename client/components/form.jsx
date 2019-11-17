5d85e76274404f756708c7cee21c47c02b19018a
“silkyh13” <silkyh13@gmail.com>
2019-11-17 04:20:27 -0500

    can get and post

--- client/components/form.jsx
+++ client/components/form.jsx
@@ -2,28 +2,52 @@ import React from 'react';
 import styles from './styles.css';
 import axios from 'axios';
 import moment from 'moment';
+
+let convert = (date) => {
+  let saveDate = date.slice(0, 10)
+  let saveTime = ' ' + date.slice(11, 19);
+  let converted = saveDate + saveTime;
+  return converted;
+}
+
 class Form extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      currentDay: '',
-      dateStart: '',
-      dateEnd: '',
+      currentDate: '',
+      startDate: '',
+      endDate: '',
       event: '',
-      data:[]
+      data:[],
+      clickedEvent: {}
     }
-    this.handleDateStart = this.handleDateStart.bind(this);
-    this.handleDateEnd = this.handleDateEnd.bind(this);
+    this.handlestartDate = this.handlestartDate.bind(this);
+    this.handleendDate = this.handleendDate.bind(this);
     this.handleEvent = this.handleEvent.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.componentDidMount = this.componentDidMount.bind(this);
+    this.onClick = this.onClick.bind(this);
   }
+  onClick(event) {
+    let element = event.target;
 
-  handleDateStart(event) {
-    this.setState({dateStart: event.target.value});
+    let start = convert(element.getAttribute('start_date'));
+    let end = convert(element.getAttribute('end_date'));
+
+    this.setState({
+      clickedEvent: {
+        start: start,
+        end: end,
+        event: element.getAttribute('event')
+      }
+    })
+
   }
-  handleDateEnd(event) {
-    this.setState({dateEnd: event.target.value});
+  handlestartDate(event) {
+    this.setState({startDate: event.target.value});
+  }
+  handleendDate(event) {
+    this.setState({endDate: event.target.value});
   }
   handleEvent(event) {
     this.setState({event: event.target.value});
@@ -31,14 +55,9 @@ class Form extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    let convert = (date) => {
-      let saveDate = date.slice(0, 10)
-      let saveTime = ' ' + date.slice(11, 19);
-      let converted = saveDate + saveTime;
-      return converted;
-    }
-    let start = convert(this.state.dateStart);
-    let end = convert(this.state.dateEnd);
+
+    let start = convert(this.state.startDate);
+    let end = convert(this.state.endDate);
     axios.post('/api/schedule', {
       event_start: start,
       event_end: end,
@@ -60,7 +79,7 @@ class Form extends React.Component {
 
     axios.get('/api/schedule')
     .then((results) => {
-      this.setState({data: results.data, currentDay: day}, () => console.log('your mom', this.state.currentDay) )
+      this.setState({data: results.data, currentDate: day}, () => console.log('your mom', this.state.currentDate) )
     })
     .catch((error) => {
       console.log(error);
@@ -68,19 +87,25 @@ class Form extends React.Component {
   }
 
   render () {
+    let start_date = this.state.clickedEvent.start;
+    let end_date = this.state.clickedEvent.end;
+    let event = this.state.clickedEvent.event;
     return (
       <div>
+        <div>
+          start:{start_date} end:{end_date} event:{event}
+        </div>
 
         <form onSubmit={this.handleSubmit}>
 
           <label>
             Start:
-            <input type="datetime-local" value={this.state.dateStart || this.state.currentDay} min={this.state.currentDay}  onChange={this.handleDateStart}></input>
+            <input type="datetime-local" value={this.state.startDate || this.state.currentDate} min={this.state.currentDate}  onChange={this.handlestartDate}></input>
           </label>
 
           <label>
             End:
-            <input type="datetime-local" value={this.state.dateEnd || this.state.currentDay} min={this.state.currentDay} onChange={this.handleDateEnd}></input>
+            <input type="datetime-local" value={this.state.endDate || this.state.currentDate} min={this.state.currentDate} onChange={this.handleendDate}></input>
           </label>
 
           <label>
@@ -92,7 +117,7 @@ class Form extends React.Component {
         </form>
 
         <ol>
-          {this.state.data.map((event, index) => <li key= {index}>
+          {this.state.data.map((event, index) => <li key= {index} onClick={this.onClick} start_date={event.event_start} end_date={event.event_end} event={event.event_description}>
             {event.event_description}
           </li>)}
         </ol>
